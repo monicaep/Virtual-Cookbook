7209f43242e5114d53eb0c395e3d17e9214f9f54
monicaep <mep823@gmail.com>
2020-04-08 17:42:07 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDF2O7zX11apfiaxDDLnQd7SgIoS1BPgIE",
+  authDomain: "virtual-cookbook.firebaseapp.com",
+  databaseURL: "https://virtual-cookbook.firebaseio.com",
+  projectId: "virtual-cookbook",
+  storageBucket: "virtual-cookbook.appspot.com",
+  messagingSenderId: "1093060911114",
+  appId: "1:1093060911114:web:a744e1a03c32aed8947ff3",
+  measurementId: "G-XJ0ZPDG7E8"
+};
+
+firebase.initiatlizeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
